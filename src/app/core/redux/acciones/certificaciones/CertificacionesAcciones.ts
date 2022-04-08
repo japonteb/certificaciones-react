ee20435fc8fd37cf870ef91fbfd4537a462dbbed
Javier Aponte <javier.aponte@ceiba.com.co>
2022-04-08 08:22:13 -0500

    Ajustes para solucionar error al agregar la certificación

--- src/app/core/redux/acciones/certificaciones/CertificacionesAcciones.ts
+++ src/app/core/redux/acciones/certificaciones/CertificacionesAcciones.ts
@@ -66,7 +66,7 @@ export function agregarNuevaCertificacionAsync(certificacion: Certificacion) {
       .then((respuesta: any) => {
         dispacth(
           agregarNuevaCertificacion(
-            certificacion,
+            respuesta.data,
             'La certificación fue almacenada con éxito'
           )
         );
